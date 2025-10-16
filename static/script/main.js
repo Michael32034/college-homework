9154964171b7f15c7f7e331d90a690301b604815
Mikeinnet <misa@email.com>
2025-10-16 10:48:40 +0300

    Creating main content start-page and practical work 1

---
+++ static/script/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById('practical_1').addEventListener('click', () => {
+    window.location.href = '/practical_1';
+  })
+});
